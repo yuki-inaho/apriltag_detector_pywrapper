d04a944effc3ca0259ff2c018f9f61c01d6c43d4
Yuki Yoshikawa <yoshikawa@inaho.co>
2020-06-24 12:12:42 +0900

    Format arrangement

--- setup.py
+++ setup.py
@@ -26,40 +26,44 @@ for elem in cvlibs_pkgcfg_list:
         pass
 
 
-sources = ["april_detector_pywrapper.pyx", "src/example/april_detector_manager.cpp"] + glob.glob("src/*.cc")
+sources = ["april_detector_pywrapper.pyx",
+           "src/example/april_detector_manager.cpp"] + glob.glob("src/*.cc")
 
 setup(
-    name = "april_detector_pywrapper",
+    name="april_detector_pywrapper",
     version='1.0.0',
     description='Python wrapper of april detector(based on ethz_apriltag2)',
     author='yuki-inaho',
-    ext_modules = cythonize(
-                 [
-                    Extension("april_detector_pywrapper",
-                        sources=sources,
-                        include_dirs=[numpy.get_include(),
-                                        os.path.join(sys.prefix, 'include', 'opencv2'),
-                                        "/usr/include/eigen3",
-                                        os.path.join(os.getcwd(), 'include'),
-                                        ],
-                        library_dirs=lib_dirs,
-                        libraries=cvlibs,
-                        extra_compile_args=["-std=gnu++11", "-O3", "-fopenmp"],
-                        extra_link_args=["-fopenmp"],
-                        language="c++",
-                    ),
+    ext_modules=cythonize(
+        [
+            Extension("april_detector_pywrapper",
+                      sources=sources,
+                      include_dirs=[numpy.get_include(),
+                                    os.path.join(
+                                        sys.prefix, 'include', 'opencv2'),
+                                    "/usr/include/eigen3",
+                                    os.path.join(
+                          os.getcwd(), 'include'),
+                      ],
+                      library_dirs=lib_dirs,
+                      libraries=cvlibs,
+                      extra_compile_args=["-std=gnu++11", "-O3", "-fopenmp"],
+                      extra_link_args=["-fopenmp"],
+                      language="c++",
+                      ),
 
-                    Extension("opencv_mat",
-                        sources=["opencv_mat.pyx"],
-                        include_dirs=[numpy.get_include(),
-                                        os.path.join(sys.prefix, 'include', 'opencv2'),
-                                        ],
-                        extra_link_args=["-std=gnu++11", "-O3"],
-                        library_dirs=lib_dirs,
-                        libraries=cvlibs,
-                        language="c++"                    
-                    )                    
-                 ],
+            Extension("opencv_mat",
+                      sources=["opencv_mat.pyx"],
+                      include_dirs=[numpy.get_include(),
+                                    os.path.join(
+                          sys.prefix, 'include', 'opencv2'),
+                      ],
+                      extra_link_args=["-std=gnu++11", "-O3"],
+                      library_dirs=lib_dirs,
+                      libraries=cvlibs,
+                      language="c++"
+                      )
+        ],
     ),
-    cmdclass = {'build_ext': build_ext},
+    cmdclass={'build_ext': build_ext},
 )
