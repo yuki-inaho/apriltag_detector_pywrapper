2ffc1554253938893b11dbcfdfa8c5b067c66fec
yuki-inaho <yoshikawa@inaho.co>
2021-12-14 18:07:42 +0900

    1. Add parse_args() to demo.py, 2. Add some comments to demo.py

--- example/demo.py
+++ example/demo.py
@@ -1,33 +1,41 @@
-from april_detector_pywrapper import PyAprilTagDetector
-import numpy as np
 import cv2
-import pdb
+import argparse
+import numpy as np
+from april_detector_pywrapper import PyAprilTagDetector
 
-video_mode = True
 
-def main():
+def parse_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--image-path", "-i", type=str, default="")
+    parser.add_argument("--video-mode", "-v", action="store_true")
+    return parser.parse_args()
+
+""" TODO: Refactor
+    TODO: Fix getDetectedImage or input image dealing (RGB output -> BGR output)
+"""
+def main(image_path, video_mode):
     if not video_mode:
-        img = cv2.imread("/home/inaho-04/Project/apriltag_detector_pywrapper/example/1596950807490547895.png")
+        img = cv2.imread(image_path)
         height, width, _ = img.shape
 
         fx = 600
         fy = 600
-        cx = int(width/2)
-        cy = int(height/2)
+        cx = int(width / 2)
+        cy = int(height / 2)
         tagSize = 0.025
 
         detector = PyAprilTagDetector()
         detector.setImageSize(width, height)
         detector.setCameraParams(fx, fy, cx, cy)
         detector.setTagSize(tagSize)
-        detector.setTagCodes("36h11".encode('utf-8'))
+        detector.setTagCodes("36h11".encode("utf-8"))
         detector.setup()
-        detector.getDetectedInfo(img, draw_flag=False)
+        detector.getDetectedInfo(cv2.cvtColor(img, cv2.COLOR_RGB2BGR), draw_flag=True)
         taginfo_list = detector.extractTagInfo()
+        print(taginfo_list)
         res_img = detector.getDetectedImage()
 
-        intrinsic_parameter_matrix = np.array(
-            [[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+        #intrinsic_parameter_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
         while cv2.waitKey(10) & 0xFF != 27:
             cv2.imshow("res_img", res_img)
 
@@ -46,21 +54,20 @@ def main():
 
         fx = 600.0
         fy = 600.0
-        cx = float(int(width/2))
-        cy = float(int(height/2))
+        cx = float(int(width / 2))
+        cy = float(int(height / 2))
         tagSize = 0.025
 
         detector = PyAprilTagDetector()
         detector.setImageSize(width, height)
         detector.setCameraParams(fx, fy, cx, cy)
         detector.setTagSize(tagSize)
-        detector.setTagCodes("36h11".encode('utf-8'))
+        detector.setTagCodes("36h11".encode("utf-8"))
         detector.setup()
 
-        intrinsic_parameter_matrix = np.array(
-            [[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+        intrinsic_parameter_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
         while cv2.waitKey(10) & 0xFF != 27:
-            for i in range(10): # for reflesh image buffer
+            for i in range(10):  # for reflesh image buffer
                 status, img = cam.read()
             if not status:
                 continue
@@ -69,11 +76,12 @@ def main():
             taginfo_list = detector.extractTagInfo()
             res_img = detector.getDetectedImage()
             cv2.imshow("res_img", res_img)
-        
+
         cv2.imwrite("res_img.png", res_img)
         cv2.waitKey(10)
         cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args.image_path, args.video_mode)
